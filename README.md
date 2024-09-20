45eb21a87a9b6176af92c40f58a1f68cdbbd0355
fiartaks <fiartaks@gmail.com>
2024-09-20 22:27:16 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# 16-js1-Light-on-off
---
+++ main.js
@@ -0,0 +1,4 @@
+//index html include these codes it is only excess
+
+onclick = "document.getElementById('light').src='img/lamba_acik.jpg' ";
+onclick = "document.getElementById('light').src='img/lamba_kapali.jpg' ";
